7aa17d19f54c939e90e8bbc1f8a5024fcbf1623b
Adityasputra <aditsputra.cv@gmail.com>
2024-12-11 00:13:02 +0700

    feat: setup server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,19 @@
+import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import { connect } from "./config/mongoose";
+import { createServer } from "node:http";
+
+export const app = express();
+export const server = createServer(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 (async () => {
   await connect();
 })();
+
+app.use("")
+
