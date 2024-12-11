7e09c015bcd4bb98d777215a4b06518af52056ea
Adityasputra <aditsputra.cv@gmail.com>
2024-12-12 02:46:51 +0700

    feat: added model for Coin

---
+++ src/models/coin.model.ts
@@ -0,0 +1,11 @@
+import { model, Schema } from "mongoose";
+import { ICoinSchema } from "../types/coin.interface";
+
+const coinSchema = new Schema<ICoinSchema>({
+  coinId: { type: String, required: true },
+  name: { type: String, required: true },
+  sysmbol: { type: String, required: true },
+  amount: { type: Number, required: true },
+});
+
+export const Coin = model("Coin", coinSchema);
